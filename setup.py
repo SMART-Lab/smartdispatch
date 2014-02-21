a37c76e501509cc948e2be4d4b04539ac09a2551
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2014-02-20 22:21:33 -0500

    Typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     author='Stanislas Lauly',
     packages=['smartDispatch'],
-    scripts=['sctipts/smartDispatch.py'],
+    scripts=['scripts/smartDispatch.py'],
     url='https://github.com/SMART-Lab/smartDispatch',
     license='LICENSE.txt',
     description='A batch job launcher for the Mammouth supercomputer.',
