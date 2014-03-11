d5b3f4e88affd517f7d3f0e5ae0da3c530755024
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-03-11 16:07:35 -0400

    Corrected missing non-PEP8 code.

--- scripts/smartDispatch.py
+++ scripts/smartDispatch.py
@@ -6,21 +6,20 @@ import math
 from subprocess import check_output
 
 
-AVAILABLE_QUEUES = \
-{
-    # Parallel Mammouth
-    'qtest@mp2':    {'coresPerNode': 24,  'maxWalltime': '00:01:00:00'},
-    'qwork@mp2':    {'coresPerNode': 24,  'maxWalltime': '05:00:00:00'},
-    'qfbb@mp2':     {'coresPerNode': 288, 'maxWalltime': '05:00:00:00'},
-    'qfat256@mp2':  {'coresPerNode': 48,  'maxWalltime': '05:00:00:00'},
-    'qfat512@mp2':  {'coresPerNode': 48,  'maxWalltime': '02:00:00:00'},
+AVAILABLE_QUEUES = {
+    # Mammouth Parallel
+    'qtest@mp2': {'coresPerNode': 24, 'maxWalltime': '00:01:00:00'},
+    'qwork@mp2': {'coresPerNode': 24, 'maxWalltime': '05:00:00:00'},
+    'qfbb@mp2': {'coresPerNode': 288, 'maxWalltime': '05:00:00:00'},
+    'qfat256@mp2': {'coresPerNode': 48, 'maxWalltime': '05:00:00:00'},
+    'qfat512@mp2': {'coresPerNode': 48, 'maxWalltime': '02:00:00:00'},
 
-    # Serial Mammouth
+    # Mammouth Série
     'qtest@ms': {'coresPerNode': 8, 'maxWalltime': '00:01:00:00'},
     'qwork@ms': {'coresPerNode': 8, 'maxWalltime': '05:00:00:00'},
     'qlong@ms': {'coresPerNode': 8, 'maxWalltime': '41:16:00:00'},
 
-    # GPU Mammouth
+    # Mammouth GPU
     # 'qwork@brume' : {'coresPerNode' : 0, 'maxWalltime' : '05:00:00:00'} # coresPerNode is variable and not relevant for this queue
 }
 
@@ -31,9 +30,9 @@ def main():
     if args.file is not None:
         # Commands are listed in a file.
         jobname = args.file.name
-        commands, logs_name = read_commands(args.file)
+        commands, logfiles_name = read_commands(args.file)
     else:
-        # Commands need to be parsed and unfold.
+        # Commands that needs to be parsed and unfolded.
         arguments = []
         for opt in args.commandAndOptions:
             opt_split = opt.split()
@@ -42,22 +41,22 @@ def main():
             arguments += [opt_split]
 
         jobname = generate_name(arguments)
-        commands, logs_name = unfold_commands(arguments)
+        commands, logfiles_name = unfold_commands(arguments)
 
-    job_dir, qsub_dir = create_job_folders(jobname)
+    job_directory, qsub_directory = create_job_folders(jobname)
 
     # Distribute equally the jobs among the QSUB files and generate those files
     nb_commands = len(commands)
-    nb_jobs = int(math.ceil(nb_commands / float(args.jobsPerNode)))
+    nb_jobs = int(math.ceil(nb_commands / float(args.nbCommandsPerNode)))
     nb_commands_per_file = int(math.ceil(nb_commands / float(nb_jobs)))
 
     qsub_filenames = []
     for i in range(nb_jobs):
         start = i * nb_commands_per_file
         end = (i + 1) * nb_commands_per_file
-        
-        qsub_filename = os.path.join(qsub_dir, 'jobCommands_' + str(i) + '.sh')
-        write_qsub_file(commands[start:end], logs_name[start:end], qsub_filename, job_dir, args.queueName, args.walltime, os.getcwd(), args.cuda)
+
+        qsub_filename = os.path.join(qsub_directory, 'jobCommands_' + str(i) + '.sh')
+        write_qsub_file(commands[start:end], logfiles_name[start:end], qsub_filename, job_directory, args.queueName, args.walltime, os.getcwd(), args.cuda)
         qsub_filenames.append(qsub_filename)
 
     # Launch the jobs with QSUB
@@ -71,75 +70,80 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-q', '--queueName', required=True, help='Queue used (ex: qwork@mp2, qfat256@mp2, qfat512@mp2)')
     parser.add_argument('-t', '--walltime', required=False, help='Set the estimated running time of your jobs using the DD:HH:MM:SS format. Note that they will be killed when this time limit is reached.')
-    parser.add_argument('-n', '--jobsPerNode', type=int, required=False, help='Set the number of jobs per nodes.')
+    parser.add_argument('-n', '--nbCommandsPerNode', type=int, required=False, help='Set the number of commands per nodes.')
     parser.add_argument('-c', '--cuda', action='store_true', help='Load CUDA before executing your code.')
-    parser.add_argument('-x', '--doNotLaunchJobs', action='store_true', help='Creates the QSUB files without launching them.')
-    parser.add_argument('-f', '--file', type=file, required=False, help='File containing commands to launch. Each command must be on a seperate line. (Replaces commandAndOptions)')
+    parser.add_argument('-x', '--doNotLaunch', action='store_true', help='Creates the QSUB files without launching them.')
+    parser.add_argument('-f', '--commandsFile', type=file, required=False, help='File containing commands to launch. Each command must be on a seperate line. (Replaces commandAndOptions)')
     parser.add_argument("commandAndOptions", help="Options for the command", nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
     # Check for invalid arguments
     if args.file is None and len(args.commandAndOptions) < 1:
         parser.error("You need to specify a command to launch.")
-    if args.queueName not in AVAILABLE_QUEUES and (args.jobsPerNode is None or args.walltime is None):
-        parser.error("Unknown queue, --jobsPerNode and --walltime must be set.")
-    
+    if args.queueName not in AVAILABLE_QUEUES and (args.nbCommandsPerNode is None or args.walltime is None):
+        parser.error("Unknown queue, --nbCommandsPerNode and --walltime must be set.")
+
     # Set queue defaults for non specified params
-    if args.jobsPerNode is None:
-        args.jobsPerNode = AVAILABLE_QUEUES[args.queueName]['coresPerNode']
+    if args.nbCommandsPerNode is None:
+        args.nbCommandsPerNode = AVAILABLE_QUEUES[args.queueName]['coresPerNode']
     if args.walltime is None:
         args.walltime = AVAILABLE_QUEUES[args.queueName]['maxWalltime']
+    
     return args
 
 
 def read_commands(fileobj):
     commands = fileobj.read().split('\n')
-    logs_name = ['{0}_command_{1}.log'.format(fileobj.name, i) for i in range(len(commands))]
-    return commands, logs_name
+    logfiles_name = ['{0}_command_{1}.log'.format(fileobj.name, i) for i in range(len(commands))]
+    return commands, logfiles_name
 
 
-def unfold_commands(arguments):
+def unfold_commands(folded_commands):
     commands = ['']
-    logs_name = ['']
+    logfiles_name = ['']
 
-    for argument in arguments:
+    # TODO: Refactor parsing
+    for argument in folded_commands:
         commands_tmp = []
-        logs_name_tmp = []
-        for valueForArg in argument:
-            for job_str, folderName in zip(commands, logs_name):
-                commands_tmp += [job_str + valueForArg + ' ']
-                valueForArg_tmp = valueForArg[-30:].split('/')[-1]  # Deal with path as parameter
-                logs_name_tmp += [valueForArg_tmp] if folderName == '' else [folderName + '-' + valueForArg_tmp]
+        logfiles_name_tmp = []
+        for argvalue in argument:
+            for job_str, folder_name in zip(commands, logfiles_name):
+                commands_tmp += [job_str + argvalue + ' ']
+                argvalue_tmp = argvalue[-30:].split('/')[-1]  # Deal with path as parameter
+                logfiles_name_tmp += [argvalue_tmp] if folder_name == '' else [folder_name + '-' + argvalue_tmp]
         commands = commands_tmp
-        logs_name = logs_name_tmp
-    return commands, logs_name
+        logfiles_name = logfiles_name_tmp
+
+    return commands, logfiles_name
 
 
 def generate_name(arguments, max_length=255):
     # Creating the folder in 'LOGS_QSUB' where the results will be saved
     name = ''
+    # TODO: Refactor name generator
     for argument in arguments:
-        str_tmp = argument[0][-30:] + ('' if len(argument) == 1 else ('-' + argument[-1][-30:]))
-        str_tmp = str_tmp.split('/')[-1]  # Deal with path as parameter
-        name += str_tmp if name == '' else ('__' + str_tmp)
+        argname = argument[0][-30:] + ('' if len(argument) == 1 else ('-' + argument[-1][-30:]))
+        argname = argname.split('/')[-1]  # Deal with path as parameter
+        name += argname if name == '' else ('__' + argname)
 
     current_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
     name = current_time + ' ' + name[:max_length-len(current_time)-1]  # No more than 256 character
     return name
+
 
 def create_job_folders(jobname):
     """Creates the folders where the logs and QSUB files will be saved."""
     path_logs = os.path.join(os.getcwd(), 'LOGS_QSUB')
     path_job_logs = os.path.join(path_logs, jobname)
     path_job_commands = os.path.join(path_job_logs, 'QSUB_commands')
-    
+
     if not os.path.exists(path_job_commands):
         os.makedirs(path_job_commands)
-    
+
     return path_job_logs, path_job_commands
 
 
-def write_qsub_file(commands, logs_name, qsub_filename, job_dir, queue, walltime, current_dir, use_cuda=False):
+def write_qsub_file(commands, logfiles_name, qsub_filename, job_directory, queue, walltime, current_directory, use_cuda=False):
     """
     Example of a line for one job for QSUB:
         cd $SRC ; python -u trainAutoEnc2.py 10 80 sigmoid 0.1 vocKL_sarath_german True True > trainAutoEnc2.py-10-80-sigmoid-0.1-vocKL_sarath_german-True-True &
@@ -155,11 +159,11 @@ def write_qsub_file(commands, logs_name, qsub_filename, job_dir, queue, walltime
         if use_cuda:
             qsub_file.write('module load cuda\n')
 
-        qsub_file.write('SRC_DIR_SMART_LAUNCHER=' + current_dir + '\n\n')
+        qsub_file.write('SRC_DIR_SMART_LAUNCHER=' + current_directory + '\n\n')
 
         command_template = "cd $SRC_DIR_SMART_LAUNCHER; {0} &> {1} &\n"
-        for command, log_name in zip(commands, logs_name):
-            qsub_file.write(command_template.format(command, os.path.join(job_dir, log_name)))
+        for command, log_name in zip(commands, logfiles_name):
+            qsub_file.write(command_template.format(command, os.path.join(job_directory, log_name)))
 
         qsub_file.write('\nwait\n')
 
