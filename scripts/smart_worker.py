3301c0c0d636c1fe1ddeb6fcac990a1b9b37d789
Mathieu Germain <mathieu.germain2@usherbrooke.ca>
2014-09-28 14:59:43 -0400

    Added step 1 for the running/resume feature.
    See #24

--- scripts/smart_worker.py
+++ scripts/smart_worker.py
@@ -6,7 +6,7 @@ import argparse
 import subprocess
 import logging
 
-from smartdispatch import utils
+from smartdispatch import utils, command_manager
 
 
 def parse_arguments():
@@ -31,6 +31,10 @@ def main():
 
     args = parse_arguments()
 
+    base_path, filename = os.path.split(args.commands_filename)
+    running_commands_filename = os.path.join(base_path, "running_" + filename)
+    finished_commands_filename = os.path.join(base_path, "finished_" + filename)
+
     while True:
         with utils.open_with_lock(args.commands_filename, 'rw+') as commands_file:
             command = commands_file.readline().strip()
@@ -52,8 +56,14 @@ def main():
                 stderr_file.write("# " + command + '\n')
                 stdout_file.flush()
                 stderr_file.flush()
+
+                # Moving the current command to the currently running command file
+                command_manager.append_command_to_file(running_commands_filename, command)
+
                 subprocess.call(command, stdout=stdout_file, stderr=stderr_file, shell=True)
 
+        # Removing the sucessfully ran command and putting it in the finished command
+        command_manager.move_running_command_to_finished(running_commands_filename, finished_commands_filename, command)
 
 if __name__ == '__main__':
     main()
