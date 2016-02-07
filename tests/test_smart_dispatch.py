5c7390156a26227d1367175fb300f9f97a1dd32a
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-02-06 23:31:42 -0500

    [TST] Addressed @mgermain's comment. Added unit test for the --onlyPending option

--- tests/test_smart_dispatch.py
+++ tests/test_smart_dispatch.py
@@ -2,6 +2,7 @@ import os
 import unittest
 import tempfile
 import shutil
+from os.path import join as pjoin
 
 from subprocess import call
 
@@ -15,13 +16,14 @@ class TestSmartdispatcher(unittest.TestCase):
         self.logs_dir = os.path.join(self.testing_dir, 'SMART_DISPATCH_LOGS')
 
         base_command = 'smart_dispatch.py --pool 10 -C 42 -q test -t 5:00 -x {0}'
-        self.launch_command = base_command.format('launch echo "1 2 3 4" "6 7 8" "9 0"')
+        self.launch_command = base_command.format('launch echo "[1 2 3 4]" "[6 7 8]" "[9 0]"')
         self.resume_command = base_command.format('resume {0}')
 
         self._cwd = os.getcwd()
         os.chdir(self.testing_dir)
 
     def tearDown(self):
+        print "Tear down"
         os.chdir(self._cwd)
         shutil.rmtree(self.testing_dir)
 
@@ -39,10 +41,49 @@ class TestSmartdispatcher(unittest.TestCase):
         call(self.launch_command, shell=True)
         batch_uid = os.listdir(self.logs_dir)[0]
 
-        # Actual test
+        # Simulate that some commands are in the running state.
+        path_job_commands = os.path.join(self.logs_dir, batch_uid, "commands")
+        pending_commands_file = pjoin(path_job_commands, "commands.txt")
+        running_commands_file = pjoin(path_job_commands, "running_commands.txt")
+        commands = open(pending_commands_file).read().strip().split("\n")
+        with open(running_commands_file, 'w') as running_commands:
+            running_commands.write("\n".join(commands[::2]) + "\n")
+        with open(pending_commands_file, 'w') as pending_commands:
+            pending_commands.write("\n".join(commands[1::2]) + "\n")
+
+        # Actual test (should move running commands back to pending).
         exit_status = call(self.resume_command.format(batch_uid), shell=True)
 
         # Test validation
         assert_equal(exit_status, 0)
         assert_true(os.path.isdir(self.logs_dir))
         assert_equal(len(os.listdir(self.logs_dir)), 1)
+        assert_equal(len(open(running_commands_file).readlines()), 0)
+        assert_equal(len(open(pending_commands_file).readlines()), len(commands))
+
+    def test_main_resume_only_pending(self):
+        # SetUp
+        call(self.launch_command, shell=True)
+        batch_uid = os.listdir(self.logs_dir)[0]
+
+        # Simulate that some commands are in the running state.
+        path_job_commands = os.path.join(self.logs_dir, batch_uid, "commands")
+        pending_commands_file = pjoin(path_job_commands, "commands.txt")
+        running_commands_file = pjoin(path_job_commands, "running_commands.txt")
+        commands = open(pending_commands_file).read().strip().split("\n")
+        with open(running_commands_file, 'w') as running_commands:
+            running_commands.write("\n".join(commands[::2]) + "\n")
+        with open(pending_commands_file, 'w') as pending_commands:
+            pending_commands.write("\n".join(commands[1::2]) + "\n")
+
+        # Actual test (should NOT move running commands back to pending).
+        command_line = self.resume_command.format(batch_uid)
+        command_line += " --onlyPending"
+        exit_status = call(command_line, shell=True)
+
+        # Test validation
+        assert_equal(exit_status, 0)
+        assert_true(os.path.isdir(self.logs_dir))
+        assert_equal(len(os.listdir(self.logs_dir)), 1)
+        assert_equal(len(open(running_commands_file).readlines()), len(commands[::2]))
+        assert_equal(len(open(pending_commands_file).readlines()), len(commands[1::2]))
