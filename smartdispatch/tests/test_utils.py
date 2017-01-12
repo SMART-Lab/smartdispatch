5492ce8a962f5934baafa3dbef4cb5d74c5e26ad
Negar Rostamzadeh <negar@helios1.helios>
2017-01-12 17:12:01 -0500

    changing name and unittest

--- smartdispatch/tests/test_utils.py
+++ smartdispatch/tests/test_utils.py
@@ -35,6 +35,10 @@ def test_generate_uid_from_string():
     assert_equal(utils.generate_uid_from_string("same text"), utils.generate_uid_from_string("same text"))
     assert_true(utils.generate_uid_from_string("same text") != utils.generate_uid_from_string("sametext"))
 
+def test_jobname_generator():
+    assertNotEqual(utils.jobname_generator("a-string-which-has-a-longer-length-nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn-than-64", 8798777), 63)
+    assertNotEqual(utils.jobname_generator("abcde",3), 7)
+    assertNotEqual(utils.jobname_generator(("",21), 3)
 
 def test_slugify():
     testing_arguments = [("command", "command"),
