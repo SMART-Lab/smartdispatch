c94f08e828d6deec453c054fd340e694ec0e0b9a
Mathieu Germain <mathieu.germain@gmail.com>
2017-02-06 16:45:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='smart-dispatch',
-    version='2.0.0',
+    version='2.0.1',
     author='Stanislas Lauly, Marc-Alexandre Côté, Mathieu Germain',
     author_email='smart-udes-dev@googlegroups.com',
     packages=['smartdispatch',
